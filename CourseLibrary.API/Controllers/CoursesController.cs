fd2d03b1237f0555eb674856896cb43993e6cbd2
akm0012 <akm0012@auburn.edu>
2020-05-22 16:46:36 -0400

    Added ability to create a course

--- CourseLibrary.API/Controllers/CoursesController.cs
+++ CourseLibrary.API/Controllers/CoursesController.cs
@@ -27,25 +27,44 @@ namespace CourseLibrary.API.Controllers
             {
                 return NotFound();
             }
-            
+
             var coursesFromRepo = _courseLibraryRepository.GetCourses(authorId);
             return Ok(_mapper.Map<IEnumerable<CourseDto>>(coursesFromRepo));
         }
-        
-        [HttpGet("{courseId}")]
+
+        [HttpGet("{courseId}", Name = "GetCourseForAuthor")]
         public ActionResult<CourseDto> GetCourseForAuthor(Guid authorId, Guid courseId)
         {
             if (!_courseLibraryRepository.AuthorExists(authorId))
             {
                 return NotFound();
             }
-            
+
             var courseFromRepo = _courseLibraryRepository.GetCourse(authorId, courseId);
             if (courseFromRepo == null)
             {
                 return NotFound();
             }
+
             return Ok(_mapper.Map<CourseDto>(courseFromRepo));
+        }
+
+        [HttpPost]
+        public ActionResult CreateCourseForAuthor(
+            Guid authorId, CourseForCreationDto courseDto)
+        {
+            if (!_courseLibraryRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var courseEntity = _mapper.Map<Entities.Course>(courseDto);
+            _courseLibraryRepository.AddCourse(authorId, courseEntity);
+            _courseLibraryRepository.Save();
+
+            var courseToReturn = _mapper.Map<CourseDto>(courseEntity);
+            
+            return CreatedAtRoute("GetCourseForAuthor", new {authorId = courseToReturn.AuthorId, courseId = courseToReturn.Id }, courseToReturn);
         }
     }
 }
